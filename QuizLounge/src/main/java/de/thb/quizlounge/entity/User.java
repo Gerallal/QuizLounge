d6779f3c59f68c1c8b51d373506362ad44403c64
SvenWeinert <sven@sihensen.de>
2025-05-22 15:20:36 +0200

    User

--- QuizLounge/src/main/java/de/thb/quizlounge/entity/User.java
+++ QuizLounge/src/main/java/de/thb/quizlounge/entity/User.java
@@ -4,12 +4,16 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//import javax.persistence.*;
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-//@Entity
-
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy =  GenerationType.IDENTITY)
+    long id;
+    String username;
+    String password;
 }
---
+++ QuizLounge/src/main/java/de/thb/quizlounge/service/UserService.java
@@ -0,0 +1,15 @@
+package de.thb.quizlounge.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import javax.persistence.*;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class UserService{
+
+}
