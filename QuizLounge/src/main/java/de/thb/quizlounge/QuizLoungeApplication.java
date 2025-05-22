671f29d814e48fe355d0296e908a5819e52c5a0f
Gerallal <dombrowe@th-brandenburg.de>
2025-05-22 14:46:04 +0200

    Changed

---
+++ QuizLounge/src/main/java/de/thb/quizlounge/QuizLoungeApplication.java
@@ -0,0 +1,13 @@
+package de.thb.quizlounge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizLoungeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizLoungeApplication.class, args);
+    }
+
+}
---
+++ QuizLounge/src/test/java/de/thb/quizlounge/QuizLoungeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.thb.quizlounge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuizLoungeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
