9e82ec191f02e59b9c3f54f135ee29f0bd41f098
alexavm268 <alexa.vonmerseky@gmx.de>
2025-05-23 00:12:55 +0200

    - add Questions to a quiz - delete Questions - edit Questions - show Questions without right Answer

--- QuizLounge/src/main/java/de/thb/quizlounge/entity/Question.java
+++ QuizLounge/src/main/java/de/thb/quizlounge/entity/Question.java
@@ -15,12 +15,13 @@ public class Question {
     @Id
     @GeneratedValue(strategy =  GenerationType.IDENTITY)
     private long id;
-    private String question;
+    private String questionname;
+    private String answer1;
+    private String answer2;
+    private String answer3;
+    private String answer4;
+    private String rightAnswer;
 
     @ManyToOne
-    @JoinColumn(name = "quiz_id")
     private Quiz quiz;
-
-    @OneToMany(mappedBy = "question", cascade = CascadeType.ALL)
-    private List<Answer> answers;
 }
