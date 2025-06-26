449ea6311875383a999967f9d4a3f59a5669bd7f
SvenWeinert <sven@sihensen.de>
2025-06-26 15:01:23 +0200

    Implemented ErrorController

--- QuizLounge/src/main/java/de/thb/quizlounge/controller/UserController.java
+++ QuizLounge/src/main/java/de/thb/quizlounge/controller/UserController.java
@@ -8,7 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
+import java.util.List;
 import javax.servlet.http.HttpSession;
 import java.util.ArrayList;
 
